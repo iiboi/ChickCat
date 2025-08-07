7bf6d09f025698bd9fc60001560de6d8c26e4d8b
iiboi <idolek28@gmail.com>
2025-08-07 17:04:01 +0300

    v0.0 Project Created

---
+++ Assets/Scripts/TestingScript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class TestingScript : MonoBehaviour
+{
+    int number = 5;
+    float AnotherNumber = 5.2f;
+    string text = "Hello World!";
+    bool Isfalse = true; 
+
+}
