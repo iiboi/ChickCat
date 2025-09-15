e48977de43af4228033fbd4e22c03a5051b9a809
iiboi <idolek28@gmail.com>
2025-09-15 17:28:35 +0300

    v0.5 Player Animation Controller Added

--- Assets/_GameAssets/Scripts/Gameplay/Player/PlayerAnimationController.cs
+++ Assets/_GameAssets/Scripts/Gameplay/Player/PlayerAnimationController.cs
@@ -1,6 +1,31 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerAnimationController : MonoBehaviour
 {
-   
+    [SerializeField] private Animator PlayerAnimator;
+
+    private PlayerController PlayerController;
+
+    private StateController StateController;
+
+    private void Awake()
+    {
+        PlayerController = GetComponent<PlayerController>();
+        StateController = GetComponent<StateController>();
+
+    }
+
+    private void SetPlayerAnimations()
+    {
+        var CurrentState = StateController.GetCurrentState();
+
+        switch (CurrentState)
+        {
+            case PlayerState.Idle:
+                PlayerAnimator.SetBool("IsDashing", false);
+                PlayerAnimator.SetBool("IsMoving", false);
+                break;
+        }
+    }
 }
