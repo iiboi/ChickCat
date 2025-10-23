3418ac1bf43c7e5bf21e59575205a9dbba7884eb
iiboi <idolek28@gmail.com>
2025-10-23 18:41:31 +0300

    v0.11 Damage System Added

---
+++ Assets/_GameAssets/Scripts/Managers/HealthManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    [SerializeField] private int MaxHealth = 3;
+
+    private int CurrentHealth;
+
+    private void Start()
+    {
+        CurrentHealth = MaxHealth;
+    }
+
+    public void Damage(int damageAmount)
+    {
+        if (CurrentHealth > 0)
+        {
+            CurrentHealth -= damageAmount;
+            //Todo: UI Animated Damage
+
+            if (CurrentHealth <= 0)
+            {
+                //Todo: Player Dead
+            }
+        }
+
+    }
+    
+    public void Heal(int healAmount)
+    {
+        
+    }
+}
